6a7a5fdcf571302a439ea30b4d3039288dfe0eb7
cody1991 <476490767@qq.com>
2016-08-04 12:52:58 +0800

    continue vue learn

---
+++ vue-tutorials/2/js/common.js
@@ -0,0 +1,23 @@
+// 全局 Vue.component('my-component',myComponent)
+
+
+var Child = Vue.extend({
+    template: '<p>This is a child component</p>'
+});
+
+var myComponent = Vue.extend({
+    template: '<div>This is my first component!<child-component></child-component></div>',
+    components: {
+        'child-component': Child
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello world'
+    },
+    components: {
+        'my-component': myComponent
+    }
+});
