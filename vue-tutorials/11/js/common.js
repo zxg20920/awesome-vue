f7a0c852876357b35df1ab6c7e5f2e6ddab01df5
cody1991 <476490767@qq.com>
2016-08-10 11:13:34 +0800

    finish vue jq learn

--- vue-tutorials/11/js/common.js
+++ vue-tutorials/11/js/common.js
@@ -37,9 +37,20 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello world',
-        gridColumns: ['customerId', 'companyName', 'contactName', 'phone'],
+        gridColumns: [{
+            name: 'customerId',
+            isKey: true
+        }, {
+            name: 'companyName'
+        }, {
+            name: 'contactName'
+        }, {
+            name: 'phone'
+        }],
         gridData: [],
-        apiUrl: 'http://211.149.193.19:8080/api/customers'
+        apiUrl: 'http://211.149.193.19:8080/api/customers',
+        item: {},
+        show: false
     },
     ready: function() {
         this.getCustomers();
@@ -51,22 +62,76 @@ var app = new Vue({
                     vm.$set('gridData', data);
                 }
 
-            ajaxHelper.get(vm.apiUrl, null, callback);
+            ajaxHelper.jsonp(vm.apiUrl, null, callback);
+        },
+        close: function() {
+            console.log(1);
+            this.show = false;
+        },
+        saveCustomer: function() {
+            this.item.customerId ? this.updateCustomer() : this.createCustomer();
+            this.show = false;
+        },
+        updateCustomer: function() {
+            var vm = this,
+                callback = function(data) {
+                    vm.getCustomers();
+                }
+
+            ajaxHelper.put(vm.apiUrl + '/' + vm.item.customerId, vm.item, callback);
+        },
+        createCustomer: function() {
+            var vm = this,
+                success = function(data) {
+                    vm.$set('item', {});
+                    vm.getCustomers();
+                }
+            ajaxHelper.post(vm.apiUrl, vm.item, success);
+        },
+        loadCustomer: function(customerId) {
+            var vm = this;
+            vm.gridData.forEach(function(item) {
+                if (item.customerId === customerId) {
+                    vm.$set('item', item);
+                    return;
+                }
+            });
+            vm.$set('show', true);
+        },
+        deleteCustomer: function(customer) {
+            var vm = this,
+                callback = function(data) {
+                    vm.getCustomers();
+                }
+
+            ajaxHelper.delete(vm.apiUrl + '/' + customer.customerId, null, callback);
         }
     },
     components: {
         'grid': {
             template: '#grid-template',
-            props: ['dataList', 'columns']
+            props: ['dataList', 'columns'],
+            methods: {
+                loadEntry: function(key) {
+                    this.$dispatch('load-entry', key);
+                },
+                deleteEntry: function(entry) {
+                    this.$dispatch('delete-entry', entry);
+                }
+            }
         },
         'dialog': {
             template: '#dialog-template',
             props: ['show'],
             methods: {
-                close: function() {
-                    this.show = false;
-                }
+
             }
         }
     }
 });
+
+app.$watch('show', function(newVal, oldVal) {
+    if (!newVal) {
+        this.item = {};
+    }
+})
