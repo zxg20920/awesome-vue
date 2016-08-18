b72afbb31e500a603034e161dbc5dd52e8df1e6f
cody1991 <476490767@qq.com>
2016-08-18 20:31:08 +0800

    finish new vue learn

--- build-an-app-with-vue-js/app.js
+++ build-an-app-with-vue-js/app.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#events',
     data: {
         event: {
-            name: '',
+            title: '',
             description: '',
             date: ''
         },
@@ -13,39 +13,45 @@ new Vue({
     },
     methods: {
         fetchEvents: function() {
-            var events = [{
-                id: 1,
-                name: 'TIFF',
-                description: 'Toronto International Film Festival',
-                date: '2015-09-10'
-            }, {
-                id: 2,
-                name: 'The Martian Premiere',
-                description: 'The Martian comes to theatres.',
-                date: '2015-10-02'
-            }, {
-                id: 3,
-                name: 'SXSW',
-                description: 'Music, film and interactive festival in Austin, TX.',
-                date: '2016-03-11'
-            }];
-
-            this.$set('events', events);
+            // var events = [{
+            //     id: 1,
+            //     name: 'TIFF',
+            //     description: 'Toronto International Film Festival',
+            //     date: '2015-09-10'
+            // }, {
+            //     id: 2,
+            //     name: 'The Martian Premiere',
+            //     description: 'The Martian comes to theatres.',
+            //     date: '2015-10-02'
+            // }, {
+            //     id: 3,
+            //     name: 'SXSW',
+            //     description: 'Music, film and interactive festival in Austin, TX.',
+            //     date: '2016-03-11'
+            // }];
+            var events = [];
+            this.$http.get('/api/events')
+                .then(function(events) {
+                    console.log(events.data);
+                    this.$set('events', events.data);
+                    console.log(events);
+                })
         },
         addEvent: function() {
-            if (this.event.name) {
-                this.events.push(this.event);
-                this.event = {
-                    name: '',
-                    description: '',
-                    date: ''
-                }
+            if (this.event.name.trim()) {
+                this.$http.post('/api/events', this.event)
+                    .then(function(res) {
+                        this.events.push(this.event);
+                        console.log('Event added!');
+                    });
             }
         },
         deleteEvent: function(index) {
             if (confirm('Are you sure you want to delete this event?')) {
-                console.log(1);
-                this.events.splice(index, 1);
+                this.$http.delete('/api/events/' + event.id)
+                    .then(function(res) {
+                        this.events.splice(index, 1);
+                    })
             }
         }
     }
---
+++ build-an-app-with-vue-js/backend/Events.js
@@ -0,0 +1,14 @@
+module.exports = [{
+    id: 1,
+    name: '視頻會議',
+    description: 'Skype with guys from NASA',
+    date: '2015-12-12'
+}, {
+    id: 2,
+    name: '去火星',
+    description: '救援马特・达蒙',
+    date: '2016-1-1'
+}, {
+    id: 3,
+    name: '记者会'
+}];
